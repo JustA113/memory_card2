b49a63bb49ac89048e88d68569553eb4100a96ba
JustA113 <129089881+JustA113@users.noreply.github.com>
2024-06-19 16:06:18 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,84 +1,47 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-    QApplication, QWidget,
-    QHBoxLayout, QVBoxLayout,
-    QLabel, QPushButton, QRadioButton,
-    QGroupBox, QButtonGroup)
+        QApplication, QWidget, QVBoxLayout, 
+        QGroupBox, QRadioButton,  
+        QPushButton, QLabel)
 
-
-class QuizApp(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('Meme Card')
-        self.init_ui()
-        self.resize(400,300)
-        self.show()
-
-    def init_ui(self):
-        self.setStyleSheet("""QWidget {
-                            font-size: 20px;}
-                            QPushButton {
-                            font-size: 20px;
-                            background-color: green;
-                            color: white;
-                            text-align: center;
-                            text-decoration: none;
-                            display: inline-block;
-                            margin: 4px 2px;
-                            cursor: pointer;
-                            border-radius: 12px;
-                            }
-                            QLabel {
-                                font-size:25px;
-                            }
-                           QGroupBox {
-                           font-size: 18px;
-                           font-weight: bold;
-                           }""")
-        self.btn_OK = QPushButton('Ответить')
-        self.question = QLabel('Самый сложный вопрос в мире!')
-
-        self.RadioGroupBox = QGroupBox('Варианты ответов')
-        self.rbtn_1 = QRadioButton('вариант 1')
-        self.rbtn_2 = QRadioButton('вариант 2')
-        self.rbtn_3 = QRadioButton('вариант 3')
-        self.rbtn_4 = QRadioButton('вариант 4')
-
-        self.RadioGroup = QButtonGroup()
-        self.RadioGroup.addButton(self.rbtn_1)
-        self.RadioGroup.addButton(self.rbtn_2)
-        self.RadioGroup.addButton(self.rbtn_3)
-        self.RadioGroup.addButton(self.rbtn_4)
-
-        self.layout_ans1 = QHBoxLayout()
-        self.layout_ans2 = QVBoxLayout()
-        self.layout_ans3 = QVBoxLayout()
-        self.layout_ans2.addWidget(self.rbtn_1)
-        self.layout_ans2.addWidget(self.rbtn_2)
-        self.layout_ans3.addWidget(self.rbtn_3)
-        self.layout_ans3.addWidget(self.rbtn_4)
-
-        self.layout_ans1.addLayout(self.layout_ans2)
-        self.layout_ans1.addLayout(self.layout_ans3)
-        self.RadioGroupBox.setLayout(self.layout_ans1)
-
-        layout_line1 = QHBoxLayout()
-        layout_line2 = QHBoxLayout()
-        layout_line3 = QHBoxLayout()
-
-        layout_line1.addWidget(self.question)
-        layout_line2.addWidget(self.RadioGroupBox)
-
-        layout_line3.addWidget(self.btn_OK, stretch=2)
-
-        layout_card = QVBoxLayout()
-        layout_card.addLayout(layout_line1,stretch=2)
-        layout_card.addLayout(layout_line2,stretch=6)
-        layout_card.addLayout(layout_line3,stretch=1)
-        layout_card.setSpacing(5)
-
-        self.setLayout(layout_card)
-
-
+#ИНТЕРФЕЙС
 app = QApplication([])
-window = QuizApp()
+window = QWidget()
+window.setWindowTitle('Memo Card')
+window.resize(400, 300)
+
+btn_OK = QPushButton('Ответить') # кнопка ответа
+lb_Question = QLabel('Самый сложный вопрос в мире!') # текст вопроса
+#ФОРМА С ВАРИАНТАМИ ОТВЕТОВ
+RadioGroupBox = QGroupBox("Варианты ответов") # группа на экране для переключателей с ответами
+
+rbtn_1 = QRadioButton('Вариант 1')
+rbtn_2 = QRadioButton('Вариант 2')
+rbtn_3 = QRadioButton('Вариант 3')
+rbtn_4 = QRadioButton('Вариант 4')
+
+layout_ans2 = QVBoxLayout() # вертикальные будут внутри горизонтального
+layout_ans2.addWidget(rbtn_1) # два ответа в первый столбец
+layout_ans2.addWidget(rbtn_2)
+layout_ans2.addWidget(rbtn_3) # два ответа во второй столбец
+layout_ans2.addWidget(rbtn_4)
+
+RadioGroupBox.setLayout(layout_ans2) # готова "панель" с вариантами ответов 
+
+#ФОРМА С РЕЗУЛЬТАТОМ
+
+#РАЗМЕЩЕНИЕ ВИДЖЕТОВ
+layout_card = QVBoxLayout()
+
+layout_card.addWidget(lb_Question)
+layout_card.addWidget(RadioGroupBox)
+
+layout_card.addWidget(btn_OK)
+#ФУНКЦИОНАЛ
+
+#ПОДПИСКИ
+
+#ЗАПУСК
+window.setLayout(layout_card)
+window.show()
 app.exec()
